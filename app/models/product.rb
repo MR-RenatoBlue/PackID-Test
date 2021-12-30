3d0742a211025c3a644db04710b1f68cbb80321a
Mr-RenatoBlue <renatomedeirosrocha@gmail.com>
2021-12-29 21:27:59 -0300

    product model validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,6 @@ class Product < ApplicationRecord
   has_many :categorizations
   has_many :categories, through: :categorizations
   
-  validates_presence_of :name, :description#
-  validates_presence_of :categories#, as: :categories_id
+  validates_presence_of :name, :description, :categories
   validates :price, presence: true, numericality: true
 end
