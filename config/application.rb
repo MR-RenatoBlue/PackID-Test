c4c22440d411ef6e22f9254cf657d0498e34c1da
Mr-RenatoBlue <renatomedeirosrocha@gmail.com>
2022-01-18 11:56:14 -0300

    added config files for actioncable

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module GRAction
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.action_cable.mount_path = '/cable'
     config.action_dispatch.rescue_responses.merge!('CanCan::AccessDenied' => :unauthorized)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
